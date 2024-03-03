7408b266f71d7b4fc9cc6794a8b4448665037a9c
jzafvt771 <ijmm7171@gmail.com>
2024-03-03 10:24:31 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm:'480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+        zaracayGreen: '#018C3E',
+        zaracayRed: '#E00712',
+      },
+      fontFamily: {
+        sans: ['Inria Sans','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
